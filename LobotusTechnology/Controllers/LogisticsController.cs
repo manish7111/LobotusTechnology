49af67429732ce041bea4826236b2b57c3af8393
Manish Reddy <mmkr7111@gmail.com>
2021-04-30 20:52:42 +0530

    Implemented a API to get the fair between two cities, swagger to test the api

---
+++ LobotusTechnology/Controllers/LogisticsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ManagerLayer;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ModelLayer;
+
+namespace LobotusTechnology.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LogisticsController : ControllerBase
+    {
+        public readonly ILogisticsManager manager;
+        public LogisticsController(ILogisticsManager manager)
+        {
+            this.manager = manager;
+        }
+        [HttpPost]
+        public ActionResult GetRatePerParcel(DataRequest request)
+        {
+            try
+            {
+                var result = this.manager.GetParcelRate(request);
+                if (result != 0)
+                {
+                    return this.Ok(new { Status = true, Message = "Parcel rate in between cities is calculated", Data = result });
+                }
+                return this.BadRequest(new { Status = false, Message = "Error while fetching details" });
+            }
+            catch (Exception e)
+            {
+                return this.NotFound(new { Status = false, Message = e.Message });
+            }
+        }
+    }
+}
---
+++ LobotusTechnology/Startup.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ManagerLayer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using RepositoryLayer;
+using RepositoryLayer.UserContext;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace LobotusTechnology
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddDbContextPool<ProblemDBContext>(options => options.UseSqlServer(Configuration.GetConnectionString("UserDbConnection")));
+            services.AddTransient<ILogisticsRepo, LogisticsRepo>();
+            services.AddTransient<ILogisticsManager, LogisticsManager>();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "LogisticsAPI", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Logistics v1");
+            });
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
